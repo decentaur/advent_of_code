b9a56acf0621551b26139cc58885df9468b243d8
Daan Veraart <daan.veraart@foaly.email>
2020-12-16 16:48:33 +0100

    day6

---
+++ day6.js
@@ -0,0 +1,29 @@
+// https://adventofcode.com/2020/day/2/
+
+const inputReader = require("./readinput");
+
+class Group {
+  awnsers = [];
+  constructor(awnserRows) {
+    for (const awnserRow of awnserRows) {
+      for (const awnser of awnserRow.split("")) {
+        this.awnsers[awnser] ? this.awnsers[awnser]++ : (this.awnsers[awnser] = 1);
+      }
+    }
+  }
+
+  getUniqueAwnsers() {
+    return Object.getOwnPropertyNames(this.awnsers).length - 1;
+  }
+}
+
+(() => {
+  const input = inputReader.getInput("./day6input.txt");
+  const dataArray = input.split("\r\n\r\n");
+  let totalunique = 0;
+  for (const row of dataArray) {
+    const group = new Group(row.split(`\r\n`));
+    totalunique += group.getUniqueAwnsers();
+  }
+  console.log(`number of yesses to awnsers in total: ${totalunique}`);
+})();
---
+++ day6part2.js
@@ -0,0 +1,41 @@
+// https://adventofcode.com/2020/day/2/
+
+const inputReader = require("./readinput");
+
+class Group {
+  awnsers = [];
+  members = 0;
+  constructor(awnserRows) {
+    for (const awnserRow of awnserRows) {
+      this.members++;
+      for (const awnser of awnserRow.split("")) {
+        this.awnsers[awnser] ? this.awnsers[awnser]++ : (this.awnsers[awnser] = 1);
+      }
+    }
+  }
+
+  getUniqueAwnsers() {
+    return Object.getOwnPropertyNames(this.awnsers).length - 1;
+  }
+
+  getAgreedAwnsers() {
+    let agreedAwnsers = 0;
+    for (const awnser in this.awnsers) {
+      if (this.awnsers[awnser] === this.members) {
+        agreedAwnsers++;
+      }
+    }
+    return agreedAwnsers;
+  }
+}
+
+(() => {
+  const input = inputReader.getInput("./day6input.txt");
+  const dataArray = input.split("\r\n\r\n");
+  let totalAgreed = 0;
+  for (const row of dataArray) {
+    const group = new Group(row.split(`\r\n`));
+    totalAgreed += group.getAgreedAwnsers();
+  }
+  console.log(`number of yesses to awnsers in total: ${totalAgreed}`);
+})();
